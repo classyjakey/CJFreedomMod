b2f5f2e001aad092f54f05f2f303bb4df0cfd2a7
Ryan <Ryan@RYAN-DESKTOP>
2013-07-07 23:27:11 +0100

    Changed /cookie to donator only, changed /ender to donator only, changed /opme to donator or admin, changed /ro to admin or donator and changed /rd to donator or admin.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_ender.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_ender.java
@@ -5,7 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
+@CommandPermissions(level = AdminLevel.DONATOR, source = SourceType.BOTH)
 @CommandParameters(description = "Goto the ender / \"The End\".", usage = "/<command>")
 public class Command_ender extends TFM_Command
 {
--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_rd.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_rd.java
@@ -5,7 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
+@CommandPermissions(level = AdminLevel.SUPERDONATOR, source = SourceType.BOTH)
 @CommandParameters(description = "Remove various server entities that may cause lag, such as dropped items, minecarts, and boats.", usage = "/<command> <carts>")
 public class Command_rd extends TFM_Command
 {
