599bc0374554549c07be82d6048641f8b50917f8
Ryan <Ryan@RYAN-DESKTOP>
2013-07-31 10:12:30 +0100

    Added donator world util's and fixed noob error with donator list!

--- src/me/StevenLawson/TotalFreedomMod/CJFM_DonatorUtils.java
+++ src/me/StevenLawson/TotalFreedomMod/CJFM_DonatorUtils.java
@@ -1,3 +1,30 @@
 package me.StevenLawson.TotalFreedomMod;
 
-// This is just a place holder at this moment, code will be added later!
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.Server;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+public class CJFM_DonatorUtils
+{
+  public static ArrayList getAvailableWorlds()
+  {
+    ArrayList availableWorlds = new ArrayList();
+    for (World world : TotalFreedomMod.server.getWorlds())
+    {
+      if (((!world.getName().equals("doantorworld"))))
+      {
+        availableWorlds.add(world);
+      }
+    }
+    return availableWorlds;
+  }
+}
