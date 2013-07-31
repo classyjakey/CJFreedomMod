b610f7215c221224c8690ac334f89b09186d5b7a
Ryan <Ryan@RYAN-DESKTOP>
2013-07-31 16:04:32 +0100

    Expell is now a donator command :)

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_expel.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_expel.java
@@ -5,7 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.ONLY_IN_GAME)
+@CommandPermissions(level = AdminLevel.SUPER, dlevel = DonatorLevel.STANDARD, source = SourceType.ONLY_IN_GAME)
 @CommandParameters(description = "Push people away from you.", usage = "/<command> [radius] [strength]")
 public class Command_expel extends TFM_Command
 {
