83062ed54da57d74c75903eafe90f8dfc53b4614
Ryan <ryan.ictclubsales@gmail.com>
2013-08-12 21:05:51 +0100

    Formatting the /dtoggle command, and did somthing to /gtfo  Removed the broken Craftbukkit import

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_dtoggle.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_dtoggle.java
@@ -12,30 +12,41 @@ import org.bukkit.plugin.Plugin;
 @CommandParameters(description = "CJFreedom Version To Manage DisguiseCraft ~ Thanks Dartheh <3.", usage = "/<command>")
 public class Command_dtoggle extends TFM_Command
 {
+
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
-    {	
-    	boolean toggled = false;
-    	boolean enabled = true;
-    	for(Plugin p : TotalFreedomMod.server.getPluginManager().getPlugins()) {
-    		if(p.getName().equalsIgnoreCase("DisguiseCraft")) {
-    			if(p.isEnabled()) {
-    				p.getPluginLoader().disablePlugin(p);
-    				enabled = false;
-    			} else {
-    				p.getPluginLoader().enablePlugin(p);
-    				enabled = true;
-    			}
-    			toggled = true;
-    		}
-    	}
-    	if(toggled) {
-    		if(!enabled) {
-    			TotalFreedomMod.server.broadcastMessage(ChatColor.RED + sender.getName() + " - Disabling DisguiseCraft");
-    		} else {
-    			TotalFreedomMod.server.broadcastMessage(ChatColor.GREEN + sender.getName() + " - Enabling DisguiseCraft");
-    		}
-    	}
+    {
+        boolean toggled = false;
+        boolean enabled = true;
+        for (Plugin p : TotalFreedomMod.server.getPluginManager().getPlugins())
+        {
+            if (p.getName().equalsIgnoreCase("DisguiseCraft"))
+            {
+                if (p.isEnabled())
+                {
+                    p.getPluginLoader().disablePlugin(p);
+                    enabled = false;
+                }
+                else
+                {
+                    p.getPluginLoader().enablePlugin(p);
+                    enabled = true;
+                }
+                toggled = true;
+            }
+        }
+        if (toggled)
+        {
+            if (!enabled)
+            {
+                TotalFreedomMod.server.broadcastMessage(ChatColor.RED + sender.getName() + " - Disabling DisguiseCraft");
+            }
+            else
+            {
+                TotalFreedomMod.server.broadcastMessage(ChatColor.GREEN + sender.getName() + " - Enabling DisguiseCraft");
+            }
+        }
         return true;
     }
+
 }
